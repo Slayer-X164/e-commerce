4d86a83731b569156a3ab33fa1e6db6847aa539b
Siddhesh <siddheshghag150@gmail.com>
2025-07-21 21:22:04 +0530

    updated loading

---
+++ app/search/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="w-screen h-screen bg-white flex items-center justify-center">
+      <h3 className="text-3xl">Loading...</h3>
+    </div>
+  );
+}
